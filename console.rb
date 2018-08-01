295113d5574f8134f2b93b266a695520fdcf70b2
Ricky <rickycorrigan@icloud.com>
2018-08-01 22:16:27 +0100

    Added Artist and Album data

--- console.rb
+++ console.rb
@@ -4,13 +4,35 @@ require_relative('./models/albums.rb')
 Albums.delete_all()
 Artists.delete_all()
 
-artist1 = Artist.new({
-  artist_name => 'Liam Gallagher'
-})
+    artist1 = Artists.new({
+      'artist_name' => 'Liam Gallagher'
+    })
 
-artist2 = Artist.new({
-  artist_name => 'Noel Gallagher'
-})
+    artist2 = Artists.new({
+      'artist_name' => 'Noel Gallagher'
+    })
 
 artist1.save
 artist2.save
+
+
+    album1 = Albums.new({
+      'album_name' => 'As You Were'
+    })
+
+    album2 = Albums.new({
+      'album_name' => 'Whats the Story Morning Glory'
+    })
+
+    album3 = Albums.new({
+      'album_name' => 'Definitely Maybe'
+    })
+
+    album4 = Albums.new({
+      'album_name' => 'Be Here Now'
+    })
+
+
+
+album1.save
+album2.save
