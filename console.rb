20baebfb8957a153d95ea780ab7bfb6d5d091e77
Ricky <rickycorrigan@icloud.com>
2018-08-01 22:23:47 +0100

    Updated Artist and Album data

--- console.rb
+++ console.rb
@@ -18,18 +18,22 @@ artist2.save
 
     album1 = Albums.new({
       'album_name' => 'As You Were'
+      'genre' => 'rock'
     })
 
     album2 = Albums.new({
       'album_name' => 'Whats the Story Morning Glory'
+      'genre' => 'rock'
     })
 
     album3 = Albums.new({
       'album_name' => 'Definitely Maybe'
+      'genre' => 'rock'
     })
 
     album4 = Albums.new({
       'album_name' => 'Be Here Now'
+      'genre' => 'rock'
     })
 
 
